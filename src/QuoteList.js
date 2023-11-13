ec97975406ada57f9c36c532c20c6502cff168d9
Skolnieks <skolnieks.pjvt@pjvt.local>
2023-11-13 11:38:04 +0200

    added

--- src/QuoteList.js
+++ src/QuoteList.js
@@ -2,27 +2,27 @@ import Quote from "./Quote.js";
 import { useEffect, useState } from "react";
 
 function QuoteList() {
-    const [data, setDatas] = useState([]);
+    const [data, setData] = useState([]);
     const [loading, setLoading] = useState(true);
     useEffect(() => {
         async function getQuotes(){
             const response = await fetch("https://dummyjson.com/quotes");
-            const data = await response.json();
-            setDatas(data.quotes);
+            const dataFromApi = await response.json();
+            setData(dataFromApi.quotes);
             setLoading(false);
         }
         getQuotes();
         
     }, []);
 
-    const quotes = data.map((data, i) => {
+    const quotesJSX = data.map((data, i) => {
         return <Quote key={i} author={data.author} quote={data.quote} />;
     });
 
     return (
         <>
             <h1>Quotes</h1>
-            {loading ? <p>Loading...</p> : quotes}
+            {loading ? <p>Loading...</p> : quotesJSX}
         </>
     );
   }
